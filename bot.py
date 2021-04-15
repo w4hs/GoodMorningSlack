84746704c07d746039c12dee5e346354870ca78c
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-04-15 16:31:46 +0600

    feat: sends message on a fixed time
    1. message send is now a fucntion
    2. an app loop checks time every hour
    3. when the preprogrammed hour is matched a "good morning" wish in a
    random language is sent

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from dotenv import load_dotenv
 from slack_sdk import WebClient
@@ -12,12 +13,23 @@ load_dotenv()
 token = os.environ['SLACK_BOT_TOKEN']
 client = WebClient(token)
 
-try:
-    message = say_good_morning(good_morning_list)
-    response = client.chat_postMessage(channel='#general', text=message)
-    assert response["message"]["text"] == message
-except SlackApiError as e:
-    # You will get a SlackApiError if "ok" is False
-    assert e.response["ok"] is False
-    assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
-    print(f"Got an error: {e.response['error']}")
+
+def send_message():
+    try:
+        message = say_good_morning(good_morning_list)
+        response = client.chat_postMessage(channel='#general', text=message)
+        assert response["message"]["text"] == message
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["ok"] is False
+        # str like 'invalid_auth', 'channel_not_found'
+        assert e.response["error"]
+        print(f"Got an error: {e.response['error']}")
+
+
+while True:
+    if time.strftime("%H") == '17':
+        send_message()
+    else:
+        print(time.strftime("%H"))
+    time.sleep(3600)
